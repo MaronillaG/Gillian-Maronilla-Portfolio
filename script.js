27d276bb39580513e61486522a51a2f46bd67020
Gillian <105639251+MaronillaG@users.noreply.github.com>
2023-06-28 10:29:12 +0200

    adding copy-to-clipboard function for email addresss in footer.

--- script.js
+++ script.js
@@ -5,46 +5,35 @@ const navLinks = document.querySelectorAll('.nav-link');
 const close = document.querySelector('#close');
 
 
+// Navigation Menu Toggle
+toggle.addEventListener('click', () => {
+    navReveal('menu clicked');
+});
 
-
-toggle.addEventListener('click', function() {
-    screen.classList.toggle('reveal');
-    toggle.classList.toggle('expand');
-    menu.classList.toggle('reveal');
-    close.classList.toggle('reveal');
-    console.log('menu clicked');
-})
-
-close.addEventListener('click', function() {
-    screen.classList.toggle('reveal');
-    toggle.classList.toggle('expand');
-    menu.classList.toggle('reveal');
-    close.classList.toggle('reveal');
-    console.log('close clicked');
+close.addEventListener('click', () => {
+    navReveal('close clicked');
 });
 
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Escape') {
-        screen.classList.toggle('reveal');
-        toggle.classList.toggle('expand');
-        navLinks.forEach(element => {
-        element.classList.toggle('reveal')
-    });
-    close.classList.toggle('reveal');
-    }
-    console.log('escape key pressed');
-}) 
+        navReveal('escape key pressed');
+}})
 
 navLinks.forEach ( link => {
-    link.addEventListener('click', function() {
-        screen.classList.toggle('reveal');
-        toggle.classList.toggle('expand');
-        menu.classList.toggle('reveal');
-        close.classList.toggle('reveal');
+    link.addEventListener('click', () => {
+        navReveal('link clicked')
     });
-    console.log('link clicked')
 });
 
+function navReveal(logMessage) {
+    screen.classList.toggle('reveal');
+    toggle.classList.toggle('expand');
+    menu.classList.toggle('reveal');
+    close.classList.toggle('reveal');
+    console.log(logMessage);
+}
+
+// fade in each section as each appears in view. 
 const observer = new IntersectionObserver((items) => {
     items.forEach((item) => {
         console.log(item)
@@ -58,3 +47,26 @@ const observer = new IntersectionObserver((items) => {
 
 const hiddenSections = document.querySelectorAll('.section-hidden');
 hiddenSections.forEach((section) => observer.observe(section));
+
+// perform copy of email address on icon click
+const email = document.querySelector('#my-email');
+const copy = document.querySelector('#copy');
+const tick = document.querySelector('#copied');
+const msg = document.querySelector('#copy-message');
+
+
+async function copyToClipboard() {
+    try {
+        navigator.clipboard.writeText(email.innerText);
+        console.log('Email address copied to clipboard')
+    } catch (error) {
+        console.error('Error copying email adress', error);
+    }
+};
+
+copy.addEventListener('click', function() {
+    copyToClipboard();
+    copy.classList.toggle('hide');
+    tick.classList.toggle('reveal');
+    msg.classList.toggle('reveal');
+});
